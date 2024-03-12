3558ae28f309bd480e0779baf3dbaf75d842be66
bohdan-rodz <162338178+bohdan-rodz@users.noreply.github.com>
2024-03-12 17:42:11 +0200

    Delete 12.03 directory

--- 12.03/task2.py
+++
@@ -1,8 +0,0 @@
-class Osoba:
-    def __init__(self, name, surname, age):
-        self.name = name
-        self.surname = surname
-        self.age = age
-
-person1 = Osoba("John", "Cena", 15)
-print(person1.name + " " + person1.surname)
